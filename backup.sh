bcdb5f8834b0f6a9a58f18dd3a4170e1838a5007
thmaz <thomasplmn@gmail.com>
2025-06-07 07:57:55 +0100

    Automated backup on Sat  7 Jun 07:57:55 BST 2025

--- backup.sh
+++ backup.sh
@@ -1,5 +1,15 @@
+#!/bin/bash 
+
+# Stop the Minecraft server
+screen -S minecraft -p 0 -X stuff "stop$(echo -ne '\r')"
+
+# Wait for the server to stop completely
+sleep 10
+
+# Navigate to the backup repository
 cd ~/mc-server-rasbian/mc-server-1.7.10/
 
+# Copy the files/folder to the repository
 cp -r world
 
 # Add changes to git
@@ -10,3 +20,6 @@ git commit -m "Automated backup on $(date)"
 
 # Push changes to GitHub
 git push origin main
+
+# Start the Minecraft server in a detached screen session
+screen -dmS minecraft java -jar server.jar nogui
---
+++ start_server.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ~/mc-server-raspbian/mc-server-1.7.10
+
+screen -dmS minecraft java -jar server.jar nogui 
